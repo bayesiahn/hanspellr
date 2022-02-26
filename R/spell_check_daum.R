f51db7448898add8fb2e140a40dd0c8584486b30
Young Ahn <bayesiahn@gmail.com>
2022-02-26 06:07:53 -0800

    Add spell_check_daum

---
+++ R/spell_check_daum.R
@@ -0,0 +1,63 @@
+DAUM_URL <- "https://dic.daum.net/grammar_checker.do"
+
+retrieve_checks_daum <- function (txt, exceptions) {
+  body <- list(
+    sentence = txt
+  )
+
+  response <- httr::POST(DAUM_URL, body = body, encode = "form")
+  errors <- rvest::html_nodes(httr::content(response, as = "parsed"), ".txt_spell")
+
+  if (length(errors) == 0)
+    return (data.table::data.table())
+
+  original <- errors %>% rvest::html_attr("data-error-input")
+  suggestion <- errors %>% rvest::html_attr("data-error-output")
+  errortype <- errors %>% rvest::html_attr("data-error-type")
+  checks <- data.table::data.table(original = original, suggestion = suggestion,
+                                   errortype = errortype)
+
+  # if there is no exception rule, just return
+  if (length(exceptions) == 0 || exceptions == "")
+    return (checks)
+
+  checks %>%
+    filter(!stringr::str_detect(original, stringr::fixed(exceptions)))
+}
+
+#' Spell checker with Daum Korean spell checker
+#'
+#' @param text Korean text to be checked
+#' @param exceptions words or phrases that checks contain to be exempted
+#'
+#' @importFrom magrittr "%>%"
+#' @return a hanspell object
+#' @export
+#'
+#' @examples
+#' spell_check_daum(wrongkortextsample)
+spell_check_daum <- function(text, exceptions = character()) {
+  # retrieve spell check results by splitting up a given text
+  text_chunks <- split_text_by_length(text, TEXT_CHUNK_LENGTH)
+  checks <- do.call(rbind,lapply(text_chunks,
+                                 function (chunk) retrieve_checks_daum(chunk, exceptions)))
+
+  # obtain a corrected text
+  text_corrected <- text
+  correction_summary <- ""
+  if (nrow(checks) > 0) {
+    replacement.dict <- checks %>%
+      dplyr::select(original, suggestion) %>%
+      tibble::deframe()
+    text_corrected <- stringr::str_replace_all(text, replacement.dict)
+    correction_summary <- get_correction_summary(checks$original, checks$suggestion)
+  }
+
+
+  out <- list(text_corrected = text_corrected, text_original = text,
+              correction_summary = correction_summary, checks = checks)
+  class(out) <- "hanspell"
+  out
+}
+
+
