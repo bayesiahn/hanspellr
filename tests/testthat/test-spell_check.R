19e87e47c8edb46e48c5d29ef0d1ca45ee66a506
Young Ahn <bayesiahn@gmail.com>
2022-02-26 22:38:44 -0800

    Add a separate wrapper for testing with timeout

--- tests/testthat/test-spell_check.R
+++ tests/testthat/test-spell_check.R
@@ -1,26 +1,31 @@
+spell_check_test <- function (text, exceptions = character()) {
+  Sys.sleep(0.5)
+  spell_check(text, exceptions = exceptions)
+}
+
 test_that("spell_check", {
-  expect_equal(spell_check("웬일이니!")$text_corrected, "웬일이니!")
-  expect_equal(spell_check("왠일이니!")$text_corrected, "웬일이니!")
+  expect_equal(spell_check_test("웬일이니!")$text_corrected, "웬일이니!")
+  expect_equal(spell_check_test("왠일이니!")$text_corrected, "웬일이니!")
 
-  expect_equal(spell_check("웬지 무섭다..")$text_corrected, "왠지 무섭다….")
-  expect_equal(spell_check("웬지 무섭다..")$text_corrected, "왠지 무섭다….")
+  expect_equal(spell_check_test("웬지 무섭다..")$text_corrected, "왠지 무섭다….")
+  expect_equal(spell_check_test("웬지 무섭다..")$text_corrected, "왠지 무섭다….")
 
-  expect_equal(spell_check("수백만의 시민들이 귀성길을 오고가고 있다.")$text_corrected,
+  expect_equal(spell_check_test("수백만의 시민들이 귀성길을 오고가고 있다.")$text_corrected,
                "수백만의 시민들이 귀성길을 오가고 있다.")
 
-  expect_equal(spell_check("인생은 아름답고 역사는 발전한다.")$text_corrected,
+  expect_equal(spell_check_test("인생은 아름답고 역사는 발전한다.")$text_corrected,
                "인생은 아름답고 역사는 발전한다.")
-  expect_equal(spell_check("인생은 아름답고 역사는발전한다.")$text_corrected,
+  expect_equal(spell_check_test("인생은 아름답고 역사는발전한다.")$text_corrected,
                "인생은 아름답고 역사는 발전한다.")
-  expect_equal(spell_check("인생은 아름답고 역사는 발전한다")$text_corrected,
+  expect_equal(spell_check_test("인생은 아름답고 역사는 발전한다")$text_corrected,
                "인생은 아름답고 역사는 발전한다.")
 
-  expect_equal(spell_check("인생은아름답고 역사는발전한다.")$text_corrected,
+  expect_equal(spell_check_test("인생은아름답고 역사는발전한다.")$text_corrected,
                "인생은 아름답고 역사는 발전한다.")
-  expect_equal(spell_check("인생은아름답고 역사는발전한다.",
+  expect_equal(spell_check_test("인생은아름답고 역사는발전한다.",
                             exceptions = "인생은아름답고")$text_corrected,
                "인생은아름답고 역사는 발전한다.")
-  expect_equal(spell_check("인생은아름답고 역사는발전한다.",
+  expect_equal(spell_check_test("인생은아름답고 역사는발전한다.",
                             exceptions = c("인생은아름답고", "역사는발전한다"))$text_corrected,
                "인생은아름답고 역사는발전한다.")
 })
--- tests/testthat/test-spell_check_daum.R
+++ tests/testthat/test-spell_check_daum.R
@@ -1,26 +1,32 @@
+spell_check_daum_test <- function (text, exceptions = character()) {
+  Sys.sleep(0.5)
+  spell_check_daum(text, exceptions = exceptions)
+}
+
+
 test_that("spell_check_daum", {
-  expect_equal(spell_check_daum("웬일이니!")$text_corrected, "웬일이니!")
-  expect_equal(spell_check_daum("왠일이니!")$text_corrected, "웬일이니!")
+  expect_equal(spell_check_daum_test("웬일이니!")$text_corrected, "웬일이니!")
+  expect_equal(spell_check_daum_test("왠일이니!")$text_corrected, "웬일이니!")
 
-  expect_equal(spell_check_daum("웬지 무섭다..")$text_corrected, "왠지 무섭다..")
-  expect_equal(spell_check_daum("웬지 무섭다..")$text_corrected, "왠지 무섭다..")
+  expect_equal(spell_check_daum_test("웬지 무섭다..")$text_corrected, "왠지 무섭다..")
+  expect_equal(spell_check_daum_test("웬지 무섭다..")$text_corrected, "왠지 무섭다..")
 
-  expect_equal(spell_check_daum("수백만의 시민들이 귀성길을 오고가고 있다.")$text_corrected,
+  expect_equal(spell_check_daum_test("수백만의 시민들이 귀성길을 오고가고 있다.")$text_corrected,
                "수백만의 시민들이 귀성길을 오고 가고 있다.")
 
-  expect_equal(spell_check_daum("인생은 아름답고 역사는 발전한다.")$text_corrected,
+  expect_equal(spell_check_daum_test("인생은 아름답고 역사는 발전한다.")$text_corrected,
                "인생은 아름답고 역사는 발전한다.")
-  expect_equal(spell_check_daum("인생은 아름답고 역사는발전한다.")$text_corrected,
+  expect_equal(spell_check_daum_test("인생은 아름답고 역사는발전한다.")$text_corrected,
                "인생은 아름답고 역사는 발전한다.")
-  expect_equal(spell_check_daum("인생은 아름답고 역사는 발전한다")$text_corrected,
+  expect_equal(spell_check_daum_test("인생은 아름답고 역사는 발전한다")$text_corrected,
                "인생은 아름답고 역사는 발전한다")
 
-  expect_equal(spell_check_daum("인생은아름답고 역사는발전한다.")$text_corrected,
+  expect_equal(spell_check_daum_test("인생은아름답고 역사는발전한다.")$text_corrected,
                "인생은 아름답고 역사는 발전한다.")
-  expect_equal(spell_check_daum("인생은아름답고 역사는발전한다.",
+  expect_equal(spell_check_daum_test("인생은아름답고 역사는발전한다.",
                                 exceptions = "인생은아름답고")$text_corrected,
                "인생은아름답고 역사는 발전한다.")
-  expect_equal(spell_check_daum("인생은아름답고 역사는발전한다.",
+  expect_equal(spell_check_daum_test("인생은아름답고 역사는발전한다.",
                                 exceptions = c("인생은아름답고", "역사는발전한다"))$text_corrected,
                "인생은아름답고 역사는발전한다.")
 })
