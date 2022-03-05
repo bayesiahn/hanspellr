45337ef37d51097732271bdfb944ad664af647fa
Young Ahn <bayesiahn@gmail.com>
2022-03-04 17:57:23 -0800

    Update spell_check.R

--- R/spell_check.R
+++ R/spell_check.R
@@ -1,8 +1,11 @@
 PNU_URL <- "http://speller.cs.pusan.ac.kr/results"
 PNU_URL2 <- "http://164.125.7.61/speller/results"
 TEXT_CHUNK_LENGTH <- 250
-PNU_MAX_TIMEOUT <- 2
-PNU_MAX_TRY <- 20
+PNU_MAX_TIMEOUT <- 10
+PNU_MAX_TRY <- 30
+COMPLEX_WORD_ERROR_PHRASE <-  "[복합어 오류]"
+UNANALYZABLE_PHRASE_ERROR_PHRASE <- "이 어절은 분석할 수 없으므로"
+
 
 is_complex_word <- function(x) stringr::str_detect(x$help, stringr::fixed(COMPLEX_WORD_ERROR_PHRASE))
 cannot_be_analyzed <- function(x) stringr::str_detect(x$help, stringr::fixed(UNANALYZABLE_PHRASE_ERROR_PHRASE))
